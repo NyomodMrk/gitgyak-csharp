ce66c2dda99cdb5614465967fb5e97cd4945e8b3
Fecske Márk <mrk.fecske@gmail.com>
2023-02-28 12:19:51 +0100

    első részfeladat

--- gitgyak-csharp/Program.cs
+++ gitgyak-csharp/Program.cs
@@ -8,8 +8,26 @@ namespace gitgyak_csharp
 {
     internal class Program
     {
+
+        static void egesz_szam()
+        {
+            Console.WriteLine("Adj meg egy egész számot: ");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adjon meg egy másik egész számot: ");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            if (szam1 > szam2)
+            {
+                Console.WriteLine("Az első szám a nagyobb");
+            }
+            else
+            {
+                Console.WriteLine("A második szám a nagyobb");
+            }
+        }
         static void Main(string[] args)
         {
+            egesz_szam();
+            Console.ReadKey();
         }
     }
 }
